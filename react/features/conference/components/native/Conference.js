ca536d4cc20270f9622090c0d1da760e02c31095
Farhan Aslam <farhan.aslam@ystl.ca>
2021-12-16 18:08:14 +0500

    2.17.66 build issues fixes

--- react/features/conference/components/native/Conference.js
+++ react/features/conference/components/native/Conference.js
@@ -304,9 +304,9 @@ class Conference extends AbstractConference<Props, *> {
 
                     <Captions onPress = { this._onClick } />
 
-                    { _shouldDisplayTileView && <Container style = { styles.displayNameContainer }>
+                    {/* { _shouldDisplayTileView && <Container style = { styles.displayNameContainer }>
                         <DisplayNameLabel participantId = { _largeVideoParticipantId } />
-                    </Container> }
+                    </Container> } */}
 
                     <LonelyMeetingExperience />
 
