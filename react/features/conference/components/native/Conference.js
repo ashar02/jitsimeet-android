4f50cf3b4f80afecf2c03090722dbd113d805a9d
Farhan Aslam <farhan.aslam@ystl.ca>
2022-04-27 22:31:27 +0500

    End-to-end Encryption text added

--- react/features/conference/components/native/Conference.js
+++ react/features/conference/components/native/Conference.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { NativeModules, SafeAreaView, StatusBar, View, Animated, PanResponder } from 'react-native';
+import { NativeModules, SafeAreaView, StatusBar, View, Animated, PanResponder, Text } from 'react-native';
 
 import { appNavigate } from '../../../app/actions';
 import { PIP_ENABLED, FULLSCREEN_ENABLED, getFeatureFlag } from '../../../base/flags';
@@ -358,6 +358,7 @@ class Conference extends AbstractConference<Props, *> {
                 <SafeAreaView
                     pointerEvents = 'box-none'
                     style = { styles.navBarSafeView }>
+                     <Text style={{color: '#C4C4C4', textAlign: 'center', fontSize:12, lineHeight: 12, marginTop: 5}}>End-to-end Encrypted</Text>
                     <NavigationBar />
                     { this._renderNotificationsContainer() }
                     <KnockingParticipantList />
